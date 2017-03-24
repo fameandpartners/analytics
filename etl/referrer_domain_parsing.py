8568e956f288d33a50dff090fdc0172e784048fc
Peter Morrow <morrow.peter123@gmail.com>
2017-03-24 15:54:08 -0700

    add date for time series analysis

--- etl/referrer_domain_parsing.py
+++ etl/referrer_domain_parsing.py
@@ -9,11 +9,12 @@ conn = psql.connect(
 cur = conn.cursor()
 
 cur.execute("""
-SELECT referrer, COUNT(*)
+SELECT referrer, created_at::DATE visit_date, COUNT(*)
 FROM marketing_user_visits
 WHERE LOWER(utm_source) = 'linkshare'
 	AND referrer IS NOT NULL
-GROUP BY referrer
+	AND created_at IS NOT NULL 
+GROUP BY referrer, created_at::DATE
 """)
 
 results = cur.fetchall()
@@ -24,13 +25,13 @@ domains = []
 for record in results:
 	parsed_uri = urlparse(record[0])
 	domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-	domains.append((domain, record[0], record[1]))
+	domains.append((domain, record[0], record[1], record[2]))
 
 import pandas as pd
 
 domain_df = pd.DataFrame(domains)
-domain_df.columns = ['domain','full_url','count']
-domain_df.to_csv('~/Documents/Data/referrer_domain_parsed.csv')
+domain_df.columns = ['domain','full_url','visit_date','count']
+domain_df.to_csv('~/Documents/Data/referrer_domain_parsed.csv', index = False)
 
 conn.close()
 cur.close()
