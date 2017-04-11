5f8747bfdc87069cc50d2a4cca9e17c954f5021c
Peter Morrow <morrow.peter123@gmail.com>
2017-04-11 16:39:16 -0700

    Finished and sent first analysis

--- analyses/customization_impact.R
+++ analyses/customization_impact.R
@@ -1,5 +1,10 @@
-source("~/Documents/Code/R/fp_init.R")
+# Connect to DB and Pull Data
+setwd("~/Documents/Code/analytics/ecommerce-performance")
+source("~/Documents/Code/analytics/ecommerce-performance/global.R")
+setwd("~/Documents/Data")
+source("~/Documents/Code/R/customization_value parsing.R")
 
+# How many dresses can be customized?
 tbl(fp_con, sql(paste(
     "SELECT COALESCE(c.has_cust, 0) has_customizations, COUNT(*)",
     "FROM spree_orders o",
@@ -23,48 +28,57 @@ tbl(fp_con, sql(paste(
 # 0.995072897
 # 99.5% of dresses sold can be customized
 
+customization_values %>% count(price) %>% mutate(n / sum(n))
+# 24% of customizations are free
 
-setwd("~/Documents/Code/analytics/ecommerce-performance")
-start_time <- now()
-source("~/Documents/Code/analytics/ecommerce-performance/global.R")
-now() - start_time
-setwd("~/Documents/Data")
+customization_values %>% group_by(product_id) %>% summarise(customizations = n()) %>% count(customizations) %>% mutate(n / sum(n))
+# 98% of products have 3 customizations available
 
-
-
-# Almost identical Gross Revenue figures
-# Customized dresses are no more profitable than non-customized dresses
-
-source("~/Documents/Code/R/customization_value parsing.R")
-
-customized_sales <- products_sold %>%
-    filter(year(order_date) == 2016) %>%
+# Paid Customizations vs. Free Customizations timeseries
+products_sold %>% 
+    filter(year(order_date) >= 2016 & order_date < as.Date("2017-04-01")) %>%
     left_join(line_item_customizations, by = "line_item_id") %>%
     mutate(customization_price_usd = customization_price * ifelse(currency == "AUD", 0.76, 1)) %>%
-    group_by(cust_code = ifelse(physically_customized == 1, "Customized","Not Customized"))
+    group_by(`Order Month` = order_year_month) %>%
+    summarise(`Customization Rate` = sum(physically_customized) / n(),
+              `Paid Customization Rate` = sum(physically_customized & coalesce(customization_price_usd, 0) > 0)/n(),
+              `Free Customization Rate` = sum(physically_customized & coalesce(customization_price_usd, 0) <= 0)/n()) %>%
+    ggplot(aes(x = `Order Month`)) +
+    geom_line(aes(y = `Customization Rate`, color = "Customization Rate"), group = 1) +
+    geom_line(aes(y = `Paid Customization Rate`, color = "Paid Customization Rate"), group = 1) +
+    geom_line(aes(y = `Free Customization Rate`, color = "Free Customization Rate", group = 1)) + 
+    scale_y_continuous(labels = percent, limits = c(0, 0.3)) +
+    theme_minimal() +
+    theme(axis.title.y = element_blank(), 
+          legend.title = element_blank())
 
-customized_sales %>%
-    summarise(Units = n(),
-              `Average Gross Revenue` = mean(gross_revenue_usd),
-              `Average Retail Price` = mean(price_usd),
-              `Average Customization Price` = mean(coalesce(customization_price_usd, 0))) %>%
-    ungroup() %>%
-    gather(metric, value, -cust_code) %>%
-    spread(cust_code, value) %>%
-    mutate(Difference = Customized - `Not Customized`) %>%
-    write_csv("Customization Averages.csv")
+customization_values %>% 
+    filter(!is.na(price)) %>%
+    rename(customization_price = price) %>%
+    group_by(product_id) %>%
+    summarise(free_customizations_available = sum(customization_price == 0)) %>%
+    inner_join(products_sold %>%
+                   group_by(product_id) %>%
+                   summarise(units = n(),
+                             customized_units = sum(physically_customized)),
+               by = "product_id") %>%
+    group_by(free_customizations_available) %>%
+    summarise(products = n(),
+              customization_rate = sum(customized_units) / sum(units))%>%
+    write_csv("~/Documents/Data/Free Customization Distro.csv")
+# 49% of products have at least 1 free customization
+# 18% of products have 2 or more free customizations
+# 4% of products have 3 or more free customizations
 
-customized_sales %>%
-    summarise(`Gross Revenue` = sum(gross_revenue_usd),
-              `Manufacturing Cost` = sum(coalesce(manufacturing_cost, 70)),
-              `Net Manufacturing Cost` = `Gross Revenue` - `Manufacturing Cost`,
-              `Margin` = `Net Manufacturing Cost` / `Gross Revenue`) %>%
-    ungroup() %>%
-    gather(metric, value, -cust_code) %>%
-    spread(cust_code, value) %>%
-    write_csv("Customization Margins.csv")
-
-# Paid customizations
+products_sold %>% 
+    left_join(line_item_customizations, by = "line_item_id") %>%
+    filter(!is.na(customization_price)) %>% 
+    count(customization_price) %>% 
+    mutate(percent(n / sum(n))) %>%
+    write_csv("~/Documents/Data/Customization Price Distro.csv")
+# 37% of customized sales have only free customizations
+# 47% of customized sales have $10 customizations
+# 10% of customized sales have $15 customizations
 
 paid_customization_sales <- products_sold %>%
     filter(year(order_date) >= 2016) %>%
@@ -74,17 +88,6 @@ paid_customization_sales <- products_sold %>%
     group_by(order_year = year(order_date),
              cust_code = ifelse(physically_customized == 1 & coalesce(customization_price_usd, 0) > 0, 
                                 "Customized","Not Customized"))
-paid_customization_sales %>%
-    summarise(Units = n(),
-              `Average Gross Revenue` = mean(gross_revenue_usd),
-              `Average Retail Price` = mean(price_usd),
-              `Average Customization Price` = mean(coalesce(customization_price_usd, 0))) %>%
-    ungroup() %>%
-    gather(metric, value, -cust_code, -order_year) %>%
-    spread(cust_code, value) %>%
-    mutate(Difference = Customized - `Not Customized`) %>%
-    write_csv("Paid Customization Averages.csv")
-
 paid_customization_sales %>%
     summarise(`Total Gross Revenue` = sum(gross_revenue_usd),
               `Total Sales` = sum(sales_usd),
@@ -101,56 +104,27 @@ paid_customization_sales %>%
     spread(cust_code, rounded_value) %>%
     write_csv("Paid Customization Margins.csv")
 
-products_sold %>%
-    inner_join(line_item_customizations, by = "line_item_id") %>%
-    count(style_name, customization) %>%
-    mutate(percent_of_customizations = n / sum(n)) %>%
-    View("Customization Distrobution by Style")
-
-# Customizations don't really have a material impact on our
-# business' performance.
-
-# in 2017
-customized_sales <- products_sold %>%
-    filter(year(order_date) == 2017) %>%
-    left_join(line_item_customizations, by = "line_item_id") %>%
-    mutate(customization_price_usd = customization_price * ifelse(currency == "AUD", 0.76, 1)) %>%
-    group_by(cust_code = ifelse(physically_customized == 1, "Customized","Not Customized"))
-
-products_sold %>% 
-    left_join(customization_values %>%
-                  rename(customization_price = price), 
-              by = "product_id") %>%
-    filter(!is.na(customization_price)) %>% 
+style_rankings <- 
+    products_sold %>% filter(sales_usd > 0) %>%
+    filter(!is.na(manufacturing_cost)) %>%
+    mutate(net_revenue = gross_revenue_usd,
+           cogs = manufacturing_cost) %>%
     group_by(style_number) %>%
-    summarise(min_customization_price = min(customization_price),
-              max_customization_price = max(customization_price),
-              customizations_available = n_distinct(presentation),
-              last_sales_date = max(order_date)) %>%
-    summarise(at_least_1_free_customization = sum(min_customization_price == 0),
-              all_customizations_free = sum(max_customization_price == 0),
-              avg_number_of_customizations = mean(customizations_available),
-              total_styles = n())
-# 26.5% of custom styles have free customizations
+    summarise(Margin = (sum(net_revenue) - sum(cogs)) / sum(net_revenue),
+              Sales = sum(net_revenue))
 
-products_sold %>% 
-    left_join(customization_values %>%
-                  rename(customization_price = price), 
-              by = "product_id") %>%
-    filter(!is.na(customization_price) & physically_customized) %>% 
-    count(customization_price) %>% 
-    mutate(percent(n / sum(n)))
-# 40% of customized dress sales have free customizations
+style_rankings %>%
+    filter(between(Margin, 0.001, 0.999)) %>%
+    ggplot(aes(x = Margin)) +
+    geom_histogram(binwidth = 0.01) +
+    scale_x_continuous(limits = c(0.5, 0.8), 
+                       labels = percent, 
+                       breaks = seq(0.3, 0.8, 0.1)) +
+    theme_minimal() +
+    ylab("Products") +
+    xlab("Net Manufacturing Cost Margin")
 
-products_sold %>% filter(year(order_date) >= 2016 & order_date < as.Date("2017-04-01")) %>%
-    left_join(line_item_customizations, by = "line_item_id") %>%
-    mutate(customization_price_usd = customization_price * ifelse(currency == "AUD", 0.76, 1)) %>%
-    group_by(`Order Month` = order_year_month) %>%
-    summarise(`Customization Rate` = sum(physically_customized) / n(),
-              `Paid Customization Rate` = sum(physically_customized & coalesce(customization_price_usd, 0) > 0)/n()) %>%
-    ggplot(aes(x = `Order Month`)) +
-    geom_line(aes(y = `Customization Rate`, color = "Customization Rate"), group = 1) +
-    geom_line(aes(y = `Paid Customization Rate`, color = "Paid Customization Rate"), group = 1) +
-    scale_y_continuous(labels = percent, limits = c(0, 0.3)) +
-    theme(axis.title.y = element_blank(), legend.title = element_blank()) +
-    theme_minimal()
+style_rankings %>%
+    filter(between(Margin, 0.001, 0.999)) %>%
+    ggplot(aes(x = Margin, y = Sales)) +
+    geom_point()
