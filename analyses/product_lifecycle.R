98c27c682dd6553374c0a1dfb9aed113da0d3dea
Peter H <morrow.peter123@gmail.com>
2017-05-03 00:20:17 -0700

    more culling stats

--- analyses/product_lifecycle.R
+++ analyses/product_lifecycle.R
@@ -57,10 +57,11 @@ monthly_dists <- 1:12 %>%
             scale_y_continuous(limits = quantile(df$units_ordered, c(0.05,0.95)))
     })
 library(gridExtra)
-do.call("grid.arrange", c(monthly_dists, ncol=4))
+do.call("grid.arrange", c(monthly_dists, ncol=3))
 
 # Seasonality by Style
 product_rankings_per_quarter <- products_sold %>%
+    filter(order_date < today() - 30) %>%
     group_by(style_number, 
              order_year_quarter = paste(year(order_date), quarter(order_date))) %>%
     summarise(units_ordered = sum(quantity),
@@ -69,20 +70,50 @@ product_rankings_per_quarter <- products_sold %>%
     group_by(order_year_quarter) %>%
     arrange(desc(net_return_request_units)) %>%
     mutate(quarterly_ranking = dense_rank(-net_return_request_units),
-           performance_percentile = ntile(-net_return_request_units, 100) / 100,
+           performance_percentile = ntile(-net_return_request_units, 1000) / 1000,
            performance_decile = ntile(-net_return_request_units, 10) %>% formatC(flag = "0", width = 2),
-           performance_quintile = ntile(-net_return_request_units, 5)) %>%
-    filter(between(performance_percentile, 0.05, 0.95))
+           performance_quintile = ntile(-net_return_request_units, 5))
 
 quarterly_summaries <- product_rankings_per_quarter %>%
-    group_by(order_year_quarter, performance_quintile) %>%
+    group_by(order_year_quarter, performance_decile) %>%
     summarise(styles = n_distinct(style_number),
               total_net_units = sum(net_return_request_units),
               best_style = max(net_return_request_units),
               worst_style = min(net_return_request_units),
               mean_per_style = mean(net_return_request_units),
               sd_per_style = sd(net_return_request_units),
+              var_per_style = var(net_return_request_units),
               q_25 = quantile(net_return_request_units, 0.25)[[1]],
               q_75 = quantile(net_return_request_units, 0.75)[[1]],
               iqr = q_75 - q_25) %>%
     mutate(percent_of_sales = total_net_units / sum(total_net_units))
+
+top_10p_summaries <- product_rankings_per_quarter %>%
+    filter(performance_decile == "01") %>%
+    mutate(top_10p_decile = ntile(-net_return_request_units, 10)) %>%
+    group_by(order_year_quarter, top_10p_decile) %>%
+    summarise(styles = n_distinct(style_number),
+              total_net_units = sum(net_return_request_units),
+              best_style = max(net_return_request_units),
+              worst_style = min(net_return_request_units),
+              mean_per_style = mean(net_return_request_units),
+              sd_per_style = sd(net_return_request_units),
+              var_per_style = var(net_return_request_units),
+              q_25 = quantile(net_return_request_units, 0.25)[[1]],
+              q_75 = quantile(net_return_request_units, 0.75)[[1]],
+              iqr = q_75 - q_25) %>%
+    mutate(percent_of_sales = total_net_units / sum(total_net_units))
+
+style_lifecycle <- product_rankings_per_quarter %>%
+    filter(order_year_quarter != "2017 2") %>%
+    group_by(style_number) %>%
+    summarise(quarters = n(),
+              total_net_units = sum(net_return_request_units),
+              best_ranking = min(quarterly_ranking),
+              worst_ranking = max(quarterly_ranking),
+              best_percentile = min(performance_percentile),
+              worst_percentile = max(performance_percentile),
+              mean_percentile = mean(performance_percentile),
+              sd_percentile = sd(performance_percentile),
+              first_quarter = min(order_year_quarter),
+              last_quarter = max(order_year_quarter))
