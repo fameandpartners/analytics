568d8a1bd214443b96e7ade34eb4ac449c4a00b6
Peter H <morrow.peter123@gmail.com>
2017-05-25 19:33:07 -0700

    Analyze engagement cadence

--- analyses/wedding_app_sales.R
+++ analyses/wedding_app_sales.R
@@ -40,3 +40,15 @@ wedding_app_sales_cycle$purchase_to_wedding_date %>% quantile(seq(0,1,0.1))
 # Time differences in days
 #    0%    10%    20%    30%    40%    50%    60%    70%    80%    90%   100% 
 # -53.0   26.4   50.8   65.0   78.6  102.0  130.4  165.8  200.4  268.6 2634.0 
+
+engagement_cadence <- wedding_emails %>% 
+    filter(dresses > 0) %>%
+    mutate(days_till_first_dress_add = first_dress_add_date - board_create_date,
+           days_till_last_dress_add = last_dress_add_date - board_create_date)
+
+quantile(engagement_cadence$days_till_first_dress_add, seq(0,1,0.01))
+quantile(engagement_cadence$days_till_last_dress_add, seq(0,1,0.01))
+
+engagement_cadence %>%
+    filter(days_till_first_dress_add > 0 | days_till_last_dress_add > 0) %>%
+    inner_join(wedding_app_sales_cycle, by = "email")
