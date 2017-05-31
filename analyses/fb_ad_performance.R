7bd5e889fb9540241b70a3c5a51a9c11c2447951
Peter H <peterhughmorrow@gmail.com>
2017-05-31 16:21:22 -0700

    Analysis for board meeting

--- analyses/fb_ad_performance.R
+++ analyses/fb_ad_performance.R
@@ -16,11 +16,13 @@ p1 <- fb %>%
              ad_month = month(Date)) %>%
     summarise(CPC = sum(Amount_Spent_AUD * 0.75) / sum(Unique_Clicks)) %>%
     ggplot(aes(x = ad_month, y = CPC, color = ad_year)) +
-    geom_line(group = 2) + geom_point() +
-    scale_x_continuous(breaks = seq(0, 12, 1), limits = c(1,11)) +
+    geom_path() + geom_point() +
+    scale_x_continuous(breaks = seq(0, 12, 1), limits = c(1,12)) +
     scale_y_continuous(labels = scales::dollar, limits = c(0,1)) +
     theme(legend.title = element_blank(),
-          axis.title.x = element_blank()) 
+          axis.title.x = element_blank(),
+          plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Facebook Acquisitions")
 
 p2 <- fb %>% 
     filter(Date < today() - 24) %>%
@@ -29,16 +31,59 @@ p2 <- fb %>%
     summarise(sample_size = n(),
               CAC = sum(Amount_Spent_AUD * 0.75) / sum(Purchases)) %>%
     ggplot(aes(x = ad_month, y = CAC, color = ad_year)) +
-    geom_line(group = 2) + geom_point() +
+    geom_path() + geom_point() +
     scale_x_continuous(breaks = seq(1, 12, 1), limits = c(1, 12)) +
     scale_y_continuous(labels = scales::dollar, limits = c(0, 250)) +
     theme(legend.title = element_blank()) +
-    xlab("month")
+    xlab("Month")
 
 library(gridExtra)
 grid.arrange(p1, p2)
 
+p3 <- fb %>%
+    filter(Date < today() - 24) %>%
+    group_by(ad_year = year(Date) %>% as.character(), 
+             ad_month = month(Date)) %>%
+    summarise(sample_size = n(),
+              CTR = sum(Unique_Clicks) / sum(Reach)) %>%
+    ggplot(aes(x = ad_month, y = CTR, color = ad_year)) +
+    geom_path() + geom_point() +
+    scale_x_continuous(breaks = seq(1, 12, 1), limits = c(1, 12)) +
+    scale_y_continuous(labels = scales::percent, limits = c(0, 0.04)) +
+    theme(legend.title = element_blank(),
+          axis.title.x = element_blank(),
+          plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Facebook CTR & Conversions")
+
+p4 <- fb %>%
+    filter(Date < today() - 24) %>%
+    group_by(ad_year = year(Date) %>% as.character(), 
+             ad_month = month(Date)) %>%
+    summarise(sample_size = n(),
+              `Conversion Rate` = sum(Purchases) / sum(Unique_Clicks)) %>%
+    ggplot(aes(x = ad_month, y = `Conversion Rate`, color = ad_year)) +
+    geom_path() + geom_point() +
+    scale_x_continuous(breaks = seq(1, 12, 1), limits = c(1, 12)) +
+    scale_y_continuous(labels = scales::percent, limits = c(0, 0.04)) +
+    theme(legend.title = element_blank()) +
+    xlab("Month")
+
+grid.arrange(p3, p4)
+
 fb %>%
     filter(Date < today() - 24) %>%
-    summarise(sample_size = n(),
-              CTR = sum(Unique_Clicks) / sum(Reach))
+    group_by(ad_year = year(Date) %>% as.character(), 
+             ad_month = month(Date)) %>%
+    summarise(Clicks = sum(Unique_Clicks),
+              Reach = sum(Reach),
+              CTR = sum(Unique_Clicks) / sum(Reach)) %>%
+    write_csv("~/data/monthly_ctr_yoy.csv")
+
+fb %>%
+    filter(Date < today() - 24) %>%
+    group_by(ad_year = year(Date) %>% as.character(), 
+             ad_month = month(Date)) %>%
+    summarise(Clicks = sum(Unique_Clicks),
+              Purchases = sum(Purchases),
+              `Conversion Rate` = sum(Purchases) / sum(Unique_Clicks)) %>%
+    write_csv("~/data/monthly_conversions_yoy.csv")
