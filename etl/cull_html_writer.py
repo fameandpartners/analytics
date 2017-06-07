50ac6e2ccdd889f115c039ec2eb400aed704e144
Peter H <peterhughmorrow@gmail.com>
2017-06-07 15:19:02 -0700

    Simple script to write a visualization of the styles we are planning to cull

---
+++ etl/cull_html_writer.py
@@ -0,0 +1,23 @@
+# Use Python 3
+# Example args:
+# '/Users/Peter 1/Dropbox (Team Fame)/data/culling/First Cull Images 2017-06-07.csv' '/Users/Peter 1/Dropbox (Team Fame)/data/culling/First_Cull_Images.hmlt'
+from sys import argv
+
+script, source_data, html_file = argv
+
+html_out = open(html_file, 'w')
+
+html_out.write('<!DOCTYPE html><html>')
+
+import csv
+with open(source_data) as c1:
+	header, *data = csv.reader(c1)
+	for row in data:
+		attachment_width = int(row[1])
+		attachment_height = int(row[2])
+		html_out.write('<img src="{0}" height={1} width={2} </img>'.format(
+			row[3], 300, 300 * (attachment_width / attachment_height)
+			)
+		)
+
+html_out.write('<html>')
