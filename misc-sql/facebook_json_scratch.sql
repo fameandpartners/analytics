5cc4638c35022f063ac5605da05ac17068e3dafb
Peter H <peterhughmorrow@gmail.com>
2017-06-15 16:32:55 -0700

    getting closer to cookin up a good query for fb

--- misc-sql/facebook_json_scratch.sql
+++ misc-sql/facebook_json_scratch.sql
@@ -1,20 +1,54 @@
-select * from 
-	(select 
-		id ad_insight_id, 
-		facebook_ad_id,
-		json_array_elements(actions) ->> 'action_type' action_type, 
-		(json_array_elements(actions) ->> 'value')::int action_value 
-	from facebook_ad_insights) fb1 
-where fb1.action_type IN (
-	'offsite_conversion.fb_pixel_purchase',
-	'offsite_conversion.fb_pixel_add_to_cart',
-	'link_click'
-);
+SELECT
+    fb1.ad_insight_id,
+    SUM(CASE WHEN action_type = 'offsite_conversion.fb_pixel_purchase' 
+            THEN action_value ELSE 0 END) purchases,
+    SUM(CASE WHEN action_type = 'offsite_conversion.fb_pixel_add_to_cart'
+            THEN action_value ELSE 0 END) adds_to_cart
+FROM 
+    (SELECT 
+        id ad_insight_id, 
+        facebook_ad_id,
+        json_array_elements(actions) ->> 'action_type' action_type, 
+        (json_array_elements(actions) ->> 'value')::int action_value 
+    FROM facebook_ad_insights) fb1 
+WHERE fb1.action_type IN (
+    'offsite_conversion.fb_pixel_purchase',
+    'offsite_conversion.fb_pixel_add_to_cart'
+)
+GROUP BY fb1.ad_insight_id;
 
-select 
-	date_start::DATE ad_date, 
-	sum(reach) reach, 
-	sum(clicks) clicks, 
-	sum(clicks) / sum(reach) ctr 
-from facebook_ad_insights 
-group by ad_date order by ad_date;
+SELECT 
+    date_start::DATE ad_date, 
+    a.name ad_name,
+    sum(reach) reach, 
+    sum(replace(social_impressions::TEXT, '"', '')::INT) impressions,
+    sum(spend) amount_spent_aud,
+    sum(clicks) clicks
+FROM facebook_ad_insights i 
+INNER JOIN facebook_ads a
+    ON a.id = i.facebook_ad_id
+INNER JOIN (
+    SELECT
+        fb1.ad_insight_id,
+        SUM(CASE WHEN action_type = 'offsite_conversion.fb_pixel_purchase' 
+                THEN action_value ELSE 0 END) purchases,
+        SUM(CASE WHEN action_type = 'offsite_conversion.fb_pixel_add_to_cart'
+                THEN action_value ELSE 0 END) adds_to_cart
+    FROM 
+        (SELECT 
+            id ad_insight_id, 
+            facebook_ad_id,
+            json_array_elements(actions) ->> 'action_type' action_type, 
+            (json_array_elements(actions) ->> 'value')::int action_value 
+        FROM facebook_ad_insights) fb1 
+    WHERE fb1.action_type IN (
+        'offsite_conversion.fb_pixel_purchase',
+        'offsite_conversion.fb_pixel_add_to_cart'
+    )
+    GROUP BY fb1.ad_insight_id
+)
+WHERE a.name like '%\_%'
+GROUP BY ad_date, ad_name 
+ORDER BY ad_date, ad_name;
+
+select targeting -> 'publisher_platforms' platform from facebook_adsets;
