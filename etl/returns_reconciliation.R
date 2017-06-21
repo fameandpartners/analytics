e10a2292578d40a23a150b78aa9dc5ee5a058988
Peter H <peterhughmorrow@gmail.com>
2017-06-21 16:04:04 -0700

    Dates parsed and merged with db order data

--- etl/returns_reconciliation.R
+++ etl/returns_reconciliation.R
@@ -46,111 +46,89 @@ payment_lkp <-
         "WHERE p.order_id != 18804392"))) %>%
     collect() 
 
-order_returns <-
-    tbl(fp_con, sql(paste(
-        "SELECT",
-            "li.order_id,",
-            "STRING_AGG(DISTINCT o.number, ',') order_number,",
-            "STRING_AGG(DISTINCT o.state, ',') order_status,",
-            "MAX(o.completed_at::DATE) order_date,",
-            "MAX(s.shipped_at::DATE) ship_date,",
-            "COUNT(DISTINCT li.id) items,",
-            "SUM(CASE WHEN li.currency = 'AUD' ",
-                    "THEN li.price * li.quantity * 0.74",
-                    "ELSE li.price * li.quantity",
-                "END) original_amount_usd,",
-            "SUM(CASE WHEN ir.id IS NOT NULL",
-                    "THEN CASE WHEN ir.order_paid_currency = 'AUD' ",
-                        "THEN ir.refund_amount * 0.74",
-                        "ELSE ir.refund_amount END",
-                    "ELSE CASE WHEN rr.refund_currency = 'AUD' ",
-                        "THEN rr.refund_amount * 0.74",
-                        "ELSE rr.refund_amount END",
-                "END) / 100 refund_amount_usd,",
-            "STRING_AGG(DISTINCT ir.requested_action, ',') requested_action,",
-            "STRING_AGG(DISTINCT",
-                "CASE WHEN ir.id IS NOT NULL",
-                    "THEN ir.acceptance_status",
-                    "ELSE rr.acceptance_status",
-                "END, ',') acceptance_status, ",
-            "COUNT(DISTINCT ir.id) item_returns",
-        "FROM spree_line_items li",
-        "INNER JOIN spree_orders o",
-            "ON o.id = li.order_id",
-        "LEFT JOIN item_returns ir",
-            "ON ir.line_item_id = li.id",
-        "LEFT JOIN refund_requests rr",
-            "ON rr.order_id = li.order_id",
-        "LEFT JOIN spree_shipments s",
-            "ON s.order_id = o.id",
-        "WHERE o.completed_at IS NOT NULL",
-        "GROUP BY li.order_id"))) %>%
-    collect()
-
-pay_pal_aud <- read_csv(
-    "~/data/paypal_aud_jan_may_refunds.csv",
-    col_types = cols(
-        .default = col_character(),
-        Gross = col_number(),
-        Fee = col_number(),
-        Net = col_number(),
-        Balance = col_number())
-)
-pay_pal_usd <- read_csv(
-    "~/data/paypal_usd_jan_may_refunds.csv",
-    col_types = cols(
-        .default = col_character(),
-        Gross = col_number(),
-        Fee = col_number(),
-        Net = col_number(),
-        Balance = col_number())
-)
-pin <- read_csv(
-    "~/data/pin_jan_may_refunds.csv",
-    col_types = cols(
-        .default = col_character(),
-        Amount = col_number()
-    )
-)
-assembly_old <- read_csv(
-    "~/data/assembly_payments_jan_to_april_old_format.csv",
-    col_types = cols(
-        .default = col_character(),
-        `Purchase Amt` = col_number()
-    )
-)
-assembly_new <- read_csv(
-    "~/data/assembly_payments_may_new_format.csv",
-    col_types = cols(
-        .default = col_character(),
-        `Purchase Amt` = col_number()
-    )
-)
+setwd("~/code/analytics/ecommerce-performance/")
+source("~/code/analytics/ecommerce-performance/global.R")
+setwd("~/data")
 
 all_refund_transactions <- bind_rows(
     list(
-        pay_pal_aud %>% 
+        read_csv(
+            "~/data/paypal_aud_jan_may_refunds.csv",
+            col_types = cols(
+                .default = col_character(),
+                Gross = col_number(),
+                Fee = col_number(),
+                Net = col_number(),
+                Balance = col_number())) %>% 
+            transmute(response_code = `Transaction ID`, 
+                      amount = Gross,
+                      date_char = Date,
+                      date = dmy(Date)),
+        read_csv(
+            "~/data/paypal_usd_jan_may_refunds.csv",
+            col_types = cols(
+                .default = col_character(),
+                Gross = col_number(),
+                Fee = col_number(),
+                Net = col_number(),
+                Balance = col_number())) %>% 
             transmute(response_code = `Transaction ID`, 
                       amount = Gross, 
-                      date = Date),
-        pay_pal_usd %>% 
-            transmute(response_code = `Transaction ID`, 
-                      amount = Gross, 
-                      date = Date),
-        pin %>% 
+                      date_char = Date,
+                      date = mdy(Date)),
+        read_csv(
+            "~/data/pin_jan_may_refunds.csv",
+            col_types = cols(
+                .default = col_character(),
+                Amount = col_number())) %>% 
             transmute(response_code = Reference, 
-                      amount = -Amount, 
-                      date = Date),
-        assembly_old %>% 
+                      amount = Amount, 
+                      date_char = Date,
+                      date = Date %>%
+                          substr(1, 10) %>%
+                          dmy()),
+        read_csv(
+            "~/data/assembly_payments_jan_to_april_old_format.csv",
+            col_types = cols(
+                .default = col_character(),
+                `Purchase Amt` = col_number())) %>% 
             transmute(response_code = `Merchant Order #`, 
-                      amount = `Purchase Amt`, 
-                      date = `Batch Post Day`),
-        assembly_new %>%
+                      amount = `Purchase Amt`,
+                      date_char = `Batch Post Day`,
+                      date = mdy(`Batch Post Day`)),
+        read_csv(
+            "~/data/assembly_payments_may_new_format.csv",
+            col_types = cols(
+                .default = col_character(),
+                `Purchase Amt` = col_number())) %>%
             transmute(response_code = `Merchant Order Number`,
                       amount = `Purchase Amt`,
-                      date = `Activity Date`)
+                      date_char = `Activity Date`,
+                      date = mdy(`Activity Date`))
     )
 )
 
-all_refund_transactions %>%
-    anti_join(payment_lkp, by = "response_code")
+payment_lkp$response_code_source <- factor(
+    payment_lkp$response_code_source,
+    levels = c("item_returns","refund_requests","spree_payments","spree_payments_paypal")
+)
+
+matched_refunds <- all_refund_transactions %>%
+    inner_join(payment_lkp, by = "response_code") %>%
+    arrange(response_code_source) %>%
+    filter(!duplicated(paste(response_code, order_id))) %>%
+    inner_join(products_sold %>%
+                   group_by(order_id) %>%
+                   summarise(refund_requested = max(return_requested),
+                             refund_processed = max(item_returned),
+                             last_order_date = max(order_date),
+                             last_ship_date = max(ship_date),
+                             original_sales_amount = sum(sales_usd),
+                             refund_amount = sum(refund_amount) / 100) %>%
+                   mutate(estimated_ship_date = coalesce(last_ship_date, 
+                                                         last_order_date + 10)), 
+               by = "order_id")
+
+missing_refunds <- all_refund_transactions %>%
+    anti_join(matched_refunds, by = "response_code")
+    
