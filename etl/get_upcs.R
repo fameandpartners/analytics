23420496520971f79bcdb1a33775fd8862d34e16
Peter H <peterhughmorrow@gmail.com>
2017-07-26 12:48:13 -0700

    A second attempt at pulling UPCs from the db

--- etl/get_upcs.R
+++ etl/get_upcs.R
@@ -6,6 +6,11 @@ library(lubridate)
 
 source("~/code/analytics/ecommerce-performance/fp_init.R")
 
+bergen_orders <- read_csv("~/data/orders_needing_upcs.csv") %>%
+    filter(order_number %>% str_detect("^[R|r|M|E]")
+           & !str_detect(order_number, " |\\/")) %>%
+    mutate(order_number_lookup = paste0("'", order_number, "'"))
+
 order_upcs <- tbl(fp_con, sql(paste(
     "SELECT DISTINCT o.id order_id, o.number order_number, g.id upc",
     "FROM spree_line_items li",
@@ -13,33 +18,50 @@ order_upcs <- tbl(fp_con, sql(paste(
         "ON o.id = li.order_id",
     "INNER JOIN spree_variants v",
         "ON v.id = li.variant_id",
+    "INNER JOIN line_item_personalizations lip",
+        "ON lip.line_item_id = li.id",
     "INNER JOIN global_skus g",
-        "ON g.variant_id = v.id",
-    "WHERE o.completed_at IS NOT NULL"))) %>%
+        "ON g.product_id = v.product_id",
+        "AND g.color_id = lip.color_id::VARCHAR",
+        "AND g.size = lip.size",
+        "AND (lip.customization_value_ids ~ g.customisation_id",
+            "OR g.customisation_id IS NULL)",
+    "WHERE o.completed_at IS NOT NULL",
+        "AND v.deleted_at IS NULL",
+        "AND o.number IN (", paste(
+            bergen_orders$order_number_lookup,
+            collapse = ","
+        ), ")"))) %>%
     collect(n = Inf)
 
-bergen_orders <- read_csv("~/data/orders_needing_upcs.csv")
+global_skus <- tbl(fp_con, "global_skus") %>% collect()
 
 bergen_orders %>%
-    inner_join(order_asns, by = "order_number")
+    select(-order_number_lookup) %>%
+    inner_join(order_upcs, by = "order_number")
 
 setwd("~/code/analytics/ecommerce-performance")
 source("~/code/analytics/ecommerce-performance/global.R")
 setwd("~/data")
 
-bergen_orders %>%
-    inner_join(products_sold, by = "order_number") %>%
-    transmute(line_item_id,
-              order_id,
-              order_number,
-              style_number,
-              order_date,
-              ship_date,
-              sales_usd,
-              currency,
-              return_requested,
-              return_request_date = as.Date(requested_at),
-              refund_date = as.Date(refunded_at),
-              refund_amount = refund_amount / 100,
-              return_comments) %>% 
-    write_csv("~/data/Bergen Orders with Return Data.csv", na = "")
+bergen_sales <- bergen_orders %>%
+    select(-order_number_lookup) %>%
+    inner_join(products_sold, by = "order_number")
+
+unique_upc_lines <- bergen_sales %>%
+    inner_join(order_upcs, by = "order_id") %>%
+    group_by(line_item_id) %>%
+    summarise(upcs = n_distinct(upc)) %>%
+    filter(upcs == 1)
+
+unique_upcs <- bergen_sales %>%
+    inner_join(order_upcs, by = "order_id") %>%
+    group_by(upc) %>%
+    summarise(styles = n_distinct(style_number)) %>%
+    filter(styles == 1)
+
+bergen_returns %>%
+    semi_join(unique_upc_lines, by = "line_item_id") %>%
+    inner_join(order_upcs %>% select(-order_number), by = "order_id") %>%
+    semi_join(unique_upcs, by = "upc") %>%
+    write_csv("~/data/bergen_orders_with_upcs.csv", na = "")
