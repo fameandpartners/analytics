882bfbacb372f8b1a4e6931d928741a6afde32ca
Peter H <peterhughmorrow@gmail.com>
2017-07-26 10:28:00 -0700

    Add pipelines for all of our core kpis annually, quarterly and monthly

--- analyses/board_presentation.R
+++ analyses/board_presentation.R
@@ -6,7 +6,7 @@ setwd("~/code/analytics/ecommerce-performance")
 source("~/code/analytics/ecommerce-performance/global.R")
 setwd("~/data")
 
-# ---- NPS ----
+# ---- DELIGHTED ----
 nps <- read_csv("~/Dropbox (Team Fame)/data/nps/delighted-trend-total-nps_21-July-2017.csv",
                 col_types = cols(
                     .default = col_character(),
@@ -14,8 +14,33 @@ nps <- read_csv("~/Dropbox (Team Fame)/data/nps/delighted-trend-total-nps_21-Jul
                     Score = col_integer()))
 
 # ---- Company Health Check ----
-monthly_actuals_2017
+products_shipped <- products_sold %>%
+    inner_join(customer_aquisitions %>%
+                   rename(acquisition_date = date), 
+               by = "email") %>%
+    filter(is_shipped & order_state != "canceled" & year(ship_date) >= 2016)
 
+# Direct Gross Revenue and Margin
+# Annual
+gross_annual <- monthly_actuals_2017 %>%
+    group_by(`Ship Year` = ship_year) %>%
+    summarise(`Gross Revenue` = sum(gross_revenue),
+              `Gross Margin` = sum(net_sales - returns - cogs) 
+                             / sum(net_sales - returns))
+# Quarterly
+gross_quarterly <- monthly_actuals_2017 %>%
+    group_by(`Ship Year` = ship_year, 
+             `Ship Quarter` = ceiling(ship_month / 3)) %>%
+    summarise(`Gross Revenue` = sum(gross_revenue),
+              `Gross Margin` = sum(net_sales - returns - cogs) 
+                             / sum(net_sales - returns))
+# Monthly
+monthly_actuals_2017 %>%
+    ungroup() %>%
+    transmute(`Ship Year` = ship_year, `Ship Month` = ship_month,
+              `Gross Revenue` = gross_revenue, `Gross Margin` = gross_margin)
+
+# NPS
 nps_sales <- nps %>%
     rename(email = Email) %>%
     inner_join(customer_aquisitions %>%
@@ -46,16 +71,64 @@ nps_summary <- function(df){
                                      / sum(Promoter + Passive + Detractor))) %>%
         filter(total_responses > 50)
 }
-
-# Annual NPS
+# Annual
 monthly_nps_responses %>%
     group_by(acquisition_year) %>%
     nps_summary()
-
+# Quarterly
 monthly_nps_responses %>%
     group_by(acquisition_year, acquisition_quarter) %>%
     nps_summary()
-
+# Monthly
 monthly_nps_responses %>%
     group_by(acquisition_year, acquisition_quarter, acquisition_month) %>%
     nps_summary()
+
+# Repeat Rate
+customer_aquisitions <- customer_aquisitions %>%
+    rbind(products_shipped %>% 
+              filter(!return_requested) %>%
+              group_by(email) %>%
+              summarise(date = min(order_date))) %>%
+    unique()
+# Annual
+products_shipped %>%
+    filter(!return_requested) %>%
+    group_by(`Ship Year` = year(ship_date),
+             New_Repeat = ifelse(order_date <= acquisition_date, "New Customers",
+                                 "Repeat Customers")) %>%
+    summarise(Customers = n_distinct(email)) %>%
+    spread(New_Repeat, Customers) %>%
+    mutate(`Repeat Rate` = `Repeat Customers` / (`New Customers` + `Repeat Customers`))
+# Quarterly
+products_shipped %>%
+    filter(!return_requested) %>%
+    group_by(`Ship Year` = year(ship_date),
+             `Ship Quarter` = quarter(ship_date),
+             New_Repeat = ifelse(order_date <= acquisition_date, "New Customers",
+                                 "Repeat Customers")) %>%
+    summarise(Customers = n_distinct(email)) %>%
+    spread(New_Repeat, Customers) %>%
+    mutate(`Repeat Rate` = `Repeat Customers` / (`New Customers` + `Repeat Customers`))
+# Monthly
+products_shipped %>%
+    filter(!return_requested) %>%
+    group_by(`Ship Year` = year(ship_date),
+             `Ship Quarter` = quarter(ship_date),
+             `Ship Month` = month(ship_date),
+             New_Repeat = ifelse(order_date <= acquisition_date, "New Customers",
+                                 "Repeat Customers")) %>%
+    summarise(Customers = n_distinct(email)) %>%
+    spread(New_Repeat, Customers) %>%
+    mutate(`Repeat Rate` = `Repeat Customers` / (`New Customers` + `Repeat Customers`))
+# Quarterly Retention
+products_shipped %>%
+    filter(year(acquisition_date) >= 2016 & !return_requested) %>%
+    group_by(`Acquisition Year Quarter` = paste(year(acquisition_date), 
+                                                quarter(acquisition_date)),
+             ship_yq = paste(year(ship_date), quarter(ship_date))) %>%
+    summarise(Customers = n_distinct(email)) %>%
+    mutate(qtrs_since_acquisition = dense_rank(ship_yq) - 1,
+           retention = Customers / max(Customers)) %>%
+    select(-ship_yq, -Customers) %>%
+    spread(qtrs_since_acquisition, retention, fill = 0)
