00bd8800f6ee1f89a61d2d81ccb5632993253266
Peter H <peterhughmorrow@gmail.com>
2017-08-10 14:19:32 -0700

    Update logic to match ecommerce performance

--- etl/full_global.R
+++ etl/full_global.R
@@ -74,7 +74,8 @@ factory_costs <- read_csv("static-data/factory_costs.csv",
                           col_types = cols(
                               StyleNumber = col_character(),
                               `Unit Price` = col_double())) %>%
-    transmute(style_number = toupper(StyleNumber), manufacturing_cost = `Unit Price`)
+    group_by(style_number = toupper(StyleNumber)) %>%
+    summarise(manufacturing_cost = min(`Unit Price`))
 
 # ---- SHIPPING COSTS ----
 shipping_costs <- read_csv("static-data/avg_shipping_costs.csv",
@@ -100,6 +101,29 @@ all_touches <- read_csv("static-data/all_touches.csv",
                             cohort = readr::col_factor(levels = c("Prom", "Bridal", "Contemporary", "Not Assigned", ordered = TRUE))
                         )) %>%
     rename(sales_usd = revenue_usd)
+
+# ---- CUSTOMER ACQUISITIONS ----
+customer_aquisitions <- read_csv("static-data/customer_aquisitions.csv",
+                                 col_types = cols(
+                                     email = col_character(),
+                                     date = col_date(format = "")))
+
+# ---- CONNECT TO REPLICA ----
+# set db connection
+source("fp_init.R")
+
+# ---- FB Ad Images ----
+fb_images <- tbl(fp_con, sql(paste(
+    "SELECT DISTINCT ads.name ad_name, copy.image_url ad_image",
+    "FROM facebook_ads ads",
+    "INNER JOIN facebook_ad_creatives copy",
+    "ON copy.facebook_ad_id = ads.id"))) %>%
+    collect() %>%
+    group_by(ad_name) %>%
+    filter(!duplicated(str_replace_all(ad_image, "\\?(.*)", ""))) %>%
+    summarise(ad_images = paste(paste0("<img height=200px width=350px src=", 
+                                       ad_image, ">"), collapse = ""))
+
 # ---- GA & FB ----
 col_args <- function(){
     cols(.default = col_number(),
@@ -132,7 +156,12 @@ ga_fb <- read_csv("static-data/ga_fb.csv",
                       Platform = col_character(),
                       prospecting = col_logical(),
                       Date = col_date(format = ""))) %>%
-    mutate(Amount_Spent_USD = Amount_Spent_AUD * aud_to_usd)
+    mutate(Amount_Spent_USD = Amount_Spent_AUD * aud_to_usd) %>%
+    left_join(fb_images %>%
+                  rename(utm_campaign = ad_name),
+              by = "utm_campaign") %>%
+    rename(creative_no_image = creative) %>%
+    mutate(creative = coalesce(ad_images, creative_no_image))
 
 # ---- COHORTS ----
 cohort_assigments <- all_touches %>%
@@ -141,10 +170,6 @@ cohort_assigments <- all_touches %>%
 
 comp_choices <- c("Spend (USD)","Purchases","CAC","CTR","CPAC","CPL",
                   "T.O.S.","Sessions","Total Carts","Bounce Rate")
-
-# ---- CONNECT TO REPLICA ----
-# set db connection
-source("fp_init.R")
 
 # ---- SALES ----
 ordered_units <- tbl(fp_con, sql(paste(
@@ -166,6 +191,7 @@ ordered_units <- tbl(fp_con, sql(paste(
     "li.quantity,",
     "li.price,",
     "v.product_id,",
+    "v.height v_height,",
     "g.size g_size",
     "FROM spree_orders o",
     "INNER JOIN spree_line_items li",
@@ -180,7 +206,7 @@ ordered_units <- tbl(fp_con, sql(paste(
     "LEFT JOIN global_skus g",
     "ON g.sku = v.sku",
     "WHERE completed_at IS NOT NULL",
-    "AND completed_at >= '2013-12-25 00:54:42'"))) %>%
+        "AND completed_at >= '2014-01-01'"))) %>%
     collect() %>%
     group_by(order_id) %>%
     mutate(gross_extra_attributed = (item_total - sum(price)) / n(),
@@ -222,7 +248,7 @@ line_item_customizations <- customizations %>%
               customization_value_id = as.integer(customization_value_id_char))
 
 customization_values <- tbl(fp_con, sql(paste(
-    "SELECT id, presentation, price",
+    "SELECT id customization_value_id, presentation, price",
     "FROM customisation_values",
     "WHERE id IN (",
     line_item_customizations$customization_value_id %>%
@@ -247,7 +273,11 @@ products <- tbl(fp_con, sql(paste(
     "WHERE is_master",
     "GROUP BY product_id",
     ") g ON g.product_id = p.id"))) %>%
-    collect()
+    collect() %>%
+    mutate(product_live = ifelse(!hidden 
+                                 & (is.na(deleted_at) | deleted_at > today()) 
+                                 & (available_on <= today()),
+                                 "Yes", "No"))
 
 # ---- ADDRESSES ----
 addresses <- tbl(fp_con, sql(paste(
@@ -306,8 +336,9 @@ correct_shipments$line_item_id <- as.integer(correct_shipments$line_item_id)
 
 # ---- RETURNS ----
 returns <- tbl(fp_con, "item_returns") %>%
-    select(requested_at, refunded_at, line_item_id, refund_amount,
-           reason_category, reason_sub_category, acceptance_status) %>%
+    select(requested_at, refunded_at, line_item_id, refund_amount, comments,
+           reason_category, reason_sub_category, acceptance_status, factory_fault) %>%
+    rename(return_comments = comments) %>%
     filter(line_item_id %in% ordered_units$line_item_id) %>%
     collect()
 
@@ -326,7 +357,7 @@ payments <- tbl(fp_con, sql(paste(
 adjustments <- tbl(fp_con, sql(paste(
     "SELECT adjustable_id order_id, originator_type, SUM(amount) adjustments",
     "FROM spree_adjustments",
-    "WHERE amount != 0 AND eligible",
+    "WHERE amount != 0 AND eligible AND adjustable_type = 'Spree::Order'",
     "GROUP BY order_id, originator_type"))) %>%
     collect() %>%
     left_join(data_frame(originator_type = c("Spree::TaxRate","Spree::ShippingMethod","Spree::PromotionAction",NA), 
@@ -335,16 +366,25 @@ adjustments <- tbl(fp_con, sql(paste(
     select(-originator_type) %>%
     spread(adjustment_type, adjustments, fill = 0)
 
-unique_products <- unique(ordered_units$product_id[!is.na(ordered_units$product_id)])
+# ---- PROMOTIONS ----
+promotions <- tbl(fp_con, sql(paste(
+    "SELECT adjustable_id order_id, STRING_AGG(label, ' ') labels",
+    "FROM spree_adjustments",
+    "WHERE amount != 0",
+    "AND eligible",
+    "AND adjustable_type = 'Spree::Order'",
+    "AND originator_type = 'Spree::PromotionAction'",
+    "GROUP BY order_id"))) %>%
+    collect() %>%
+    mutate(coupon_code = labels %>% 
+               str_replace_all("Promotion |\\(|\\)", "")) %>%
+    select(-labels)
 
 # ---- PRODUCT TAXONS ----
 product_taxons <- tbl(fp_con, sql(paste(
     "SELECT pt.product_id, t.name taxon_name",
     "FROM spree_products_taxons pt",
-    "JOIN spree_taxons t on t.id = pt.taxon_id",
-    "WHERE pt.product_id IN (",
-    paste(unique_products, collapse = ","),
-    ")"))) %>%
+    "JOIN spree_taxons t on t.id = pt.taxon_id"))) %>%
     collect()
 
 # ---- DRESS IMAGES ----
@@ -359,8 +399,7 @@ dress_images <- tbl(fp_con, sql(paste(
     "WHERE a.attachment_width < 2000",
     "AND a.viewable_type = 'ProductColorValue'",
     "AND a.attachment_updated_at >= '2015-01-01'",
-    "AND a.attachment_file_name ilike '%front-crop%'",
-    "AND p.id IN (", paste(unique_products, collapse = ","), ")"))) %>%
+    "AND a.attachment_file_name ilike '%front-crop%'"))) %>%
     collect() %>%
     filter(!duplicated(product_id)) %>%
     mutate(
@@ -379,6 +418,14 @@ dress_images <- tbl(fp_con, sql(paste(
         )
     )
 
+# ---- SLOW AND FAST MAKING ----
+slow_fast_items <- tbl(fp_con, sql(paste(
+    "SELECT limo.line_item_id, pmo.option_type making_option",
+    "FROM line_item_making_options limo",
+    "INNER JOIN product_making_options pmo",
+    "ON pmo.id = limo.making_option_id"))) %>%
+    collect()
+
 # ---- MERGE + TRANSFORM QUERIES INTO MASTER SALES DF ----
 products_sold <- ordered_units %>%
     left_join(customizations, by = "line_item_id") %>%
@@ -389,6 +436,7 @@ products_sold <- ordered_units %>%
     left_join(returns, by = "line_item_id") %>%
     left_join(payments, by = "order_id") %>%
     left_join(adjustments, by = "order_id") %>%
+    left_join(promotions, by = "order_id") %>%
     left_join(product_taxons %>%
                   filter(taxon_name %>% tolower() %>% str_detect("mini|knee|petti|midi|ankle|maxi|long")
                          & !str_detect(taxon_name, " ")) %>%
@@ -396,11 +444,20 @@ products_sold <- ordered_units %>%
                   summarise(length = paste0(taxon_name[1] %>% str_trim() %>% substr(1, 1) %>% toupper(), 
                                             taxon_name[1] %>% str_trim() %>% substr(2, 10))),
               by = "product_id") %>%
-    left_join(cohort_assigments, by = "email") %>%
+    left_join(cohort_assigments %>% filter(!duplicated(email)), by = "email") %>%
     left_join(correct_shipments %>%
                   group_by(line_item_id) %>%
                   summarise(correct_ship_date = min(correct_ship_date)), 
               by = "line_item_id") %>%
+    left_join(slow_fast_items, by = "line_item_id") %>%
+    left_join(customer_aquisitions %>%
+                  group_by(email) %>%
+                  summarise(acquisition_date = min(date)) %>%
+                  rbind(ordered_units %>%
+                            anti_join(customer_aquisitions, by = "email") %>%
+                            group_by(email) %>%
+                            summarise(acquisition_date = min(order_date))),
+              by = "email") %>%
     group_by(order_id) %>%
     mutate(payments = coalesce(order_payments / n(), 0),
            item_total_usd = item_total * conversion_rate,
@@ -412,13 +469,12 @@ products_sold <- ordered_units %>%
     mutate(ship_date = coalesce(correct_ship_date, li_ship_date, o_ship_date),
            refund_amount_usd = (refund_amount / 100) * ifelse(currency == "AUD", aud_to_usd, 1),
            price_usd = price * ifelse(currency == "AUD", aud_to_usd, 1),
-           height = paste0(substr(lip_height, 1, 1) %>% toupper(), substr(lip_height, 2, 250)),
+           height = coalesce(paste0(substr(lip_height, 1, 1) %>% toupper(), 
+                                    substr(lip_height, 2, 250)),
+                             paste0(substr(v_height, 1, 1) %>% toupper(), 
+                                    substr(v_height, 2, 250))),
            size = coalesce(g_size, lip_size),
            return_order_id = ifelse(!is.na(acceptance_status), order_id, NA),
-           product_live = ifelse(!hidden 
-                                 & (is.na(deleted_at) | deleted_at > today()) 
-                                 & (available_on <= today()),
-                                 "Yes", "No"),
            is_shipped = !is.na(ship_date),
            return_requested = !is.na(return_order_id),
            item_returned = !is.na(refund_amount),
@@ -435,7 +491,8 @@ products_sold <- ordered_units %>%
            ship_year_month = year_month(estimated_ship_date),
            order_year_month = year_month(order_date),
            payment_processing_cost = (sales_usd * 0.029) + 0.3,
-           physically_customized = ifelse(is.na(customized), 0, customized)) %>%
+           physically_customized = ifelse(is.na(customized), 0, customized),
+           repeat_purchase = order_date > acquisition_date) %>%
     left_join(collections %>%
                   group_by(product_id) %>%
                   summarise(collection_na = paste(unique(collection_na), collapse = ", ")),
@@ -453,7 +510,11 @@ products_sold <- ordered_units %>%
     # Filter out mysterious Manual order that's not right
     filter(order_id != 32162864) %>%
     # Filter PR orders
-    filter(adjustments_total_percentage > -0.7) %>%
+    filter(email != "abbyv@fameandpartners.com") %>%
+    # Filter out reggie's test order for Stripe
+    filter(order_number != "R028450713") %>%
+    filter(between(adjustments_total_percentage, -0.7, 1)) %>%
+    filter(payments != 0) %>%
     left_join(shipping_costs, by = c("ship_year_month")) %>%
     group_by(order_id) %>%
     mutate(li_shipping_cost = coalesce(avg_order_shipping_cost / n(),
@@ -476,5 +537,151 @@ products_sold$height <- factor(
     products_sold$height,
     levels = c("Petite", "Standard", "Tall", paste0("Length", 1:6))
 )
+
+# ---- BUDGET DATA ----
+
+monthly_budget_2017 <- read_csv("static-data/direct_2017_monthly_budget.csv",
+                                col_types = "iddddddddddddddd")
+
+returns_reconciled <- read_csv("static-data/reconciled_returns.csv",
+                               col_types = cols(.default = col_number())) %>%
+    filter(ship_month <= 4)
+
+monthly_actuals_2017 <- products_sold %>%
+    filter(is_shipped & order_state != "canceled" & year(ship_date) >= 2016) %>%
+    group_by(ship_year = year(ship_date),
+             ship_month = month(ship_date)) %>%
+    summarise(gross_revenue = sum(gross_revenue_usd),
+              net_sales = sum(sales_usd),
+              units_shipped = sum(quantity),
+              cogs = sum(coalesce(manufacturing_cost, 70))
+              + sum(li_shipping_cost)
+              + sum(payment_processing_cost),
+              spree_returns = sum(coalesce(refund_amount_usd, 0)),
+              total_adjustments = sum(adjustments_usd)) %>%
+    left_join(returns_reconciled %>% select(-ship_quarter),
+              by = c("ship_year","ship_month")) %>%
+    mutate(returns = coalesce(adjusted_returns, spree_returns)) %>%
+    mutate(average_selling_price = gross_revenue / units_shipped,
+           average_unit_cogs = cogs / units_shipped,
+           return_rate = returns / gross_revenue,
+           gross_margin = (gross_revenue + total_adjustments - cogs - returns)
+           / (gross_revenue + total_adjustments - returns),
+           returns_per_unit = returns / units_shipped,
+           average_discount = abs(total_adjustments) / gross_revenue) %>%
+    select(-spree_returns, -adjusted_returns)
+# NOTES:
+# 98% of returns are processed within 90 days
+# less_than_90_days n `n/sum(n)`
+# 1 FALSE           87 0.01549145
+# 2 TRUE          5529 0.98450855
+
+# # 74% of refund request dollars are refunded
+# # This observation drives the crude estimate of 0.65 * refunds requesteds
+# # We anticipate that this rate will drop this year because of bridal returns
+# # refunded
+# # 1 0.7391868
+
+monthly_budget_actuals_2017 <- monthly_actuals_2017 %>%
+    gather(metric, value, -ship_month, -ship_year) %>%
+    mutate(year_colname = paste("actuals", ship_year, sep = "_")) %>%
+    ungroup() %>%
+    select(-ship_year) %>%
+    spread(year_colname, value) %>%
+    filter(!is.na(actuals_2017)) %>%
+    inner_join(monthly_budget_2017 %>%
+                   gather(metric, budget_2017, -ship_month),
+               by = c("ship_month","metric")) %>%
+    transmute(ship_quarter = ceiling(ship_month / 3),
+              ship_month,
+              metric,
+              actuals_2016 = round(actuals_2016, 2),
+              actuals_2017 = round(actuals_2017, 2),
+              budget_2017  = round(budget_2017, 2),
+              percent_change_yoy = (actuals_2017 - actuals_2016) / actuals_2016,
+              percent_of_budget = actuals_2017 / budget_2017) %>%
+    arrange(metric, ship_month)
+
+quarterly_budget_2017 <- monthly_budget_2017 %>%
+    group_by(ship_quarter = ceiling(ship_month / 3)) %>%
+    summarise(gross_revenue = sum(gross_revenue),
+              units_shipped = sum(units_shipped),
+              cogs = sum(cogs),
+              returns = sum(returns)) %>%
+    mutate(average_selling_price = gross_revenue / units_shipped,
+           average_unit_cogs = cogs / units_shipped,
+           return_rate = returns / gross_revenue,
+           gross_margin = (gross_revenue - returns - cogs)
+           / (gross_revenue - returns),
+           returns_per_unit = returns / units_shipped) %>%
+    gather(metric, budget_2017, -ship_quarter)
+
+quarterly_budget_actuals_2017 <- monthly_actuals_2017 %>%
+    group_by(ship_year,
+             ship_quarter = ceiling(ship_month / 3)) %>%
+    summarise(gross_revenue = sum(gross_revenue),
+              units_shipped = sum(units_shipped),
+              cogs = sum(cogs),
+              returns = sum(returns),
+              total_adjustments = sum(total_adjustments)) %>%
+    mutate(average_selling_price = gross_revenue / units_shipped,
+           average_unit_cogs = cogs / units_shipped,
+           return_rate = returns / gross_revenue,
+           gross_margin = (gross_revenue + total_adjustments - cogs - returns)
+           / (gross_revenue + total_adjustments - returns),
+           returns_per_unit = returns / units_shipped) %>%
+    gather(metric, value, -ship_quarter, -ship_year) %>%
+    mutate(year_colname = paste("actuals", ship_year, sep = "_")) %>%
+    ungroup() %>%
+    select(-ship_year) %>%
+    spread(year_colname, value) %>%
+    filter(!is.na(actuals_2017)) %>%
+    inner_join(quarterly_budget_2017, by = c("ship_quarter", "metric")) %>%
+    mutate(percent_change_yoy = (actuals_2017 - actuals_2016) / actuals_2016,
+           percent_of_budget = round(actuals_2017 / budget_2017, 4)) %>%
+    transmute(ship_quarter, metric,
+              actuals_2016 = round(actuals_2016, 2),
+              actuals_2017 = round(actuals_2017, 2),
+              budget_2017  = round(budget_2017, 2),
+              percent_change_yoy, percent_of_budget)
+
+annual_budget_2017 <- monthly_budget_2017 %>%
+    summarise(gross_revenue = sum(gross_revenue),
+              units_shipped = sum(units_shipped),
+              cogs = sum(cogs),
+              returns = sum(returns)) %>%
+    mutate(average_selling_price = gross_revenue / units_shipped,
+           average_unit_cogs = cogs / units_shipped,
+           return_rate = returns / gross_revenue,
+           gross_margin = (gross_revenue - returns - cogs)
+           / (gross_revenue - returns),
+           returns_per_unit = returns / units_shipped) %>%
+    gather(metric, budget_2017)
+
+annual_budget_actuals_2017 <- monthly_actuals_2017 %>%
+    group_by(ship_year) %>%
+    summarise(gross_revenue = sum(gross_revenue),
+              units_shipped = sum(units_shipped),
+              cogs = sum(cogs),
+              returns = sum(returns),
+              total_adjustments = sum(total_adjustments)) %>%
+    mutate(average_selling_price = gross_revenue / units_shipped,
+           average_unit_cogs = cogs / units_shipped,
+           return_rate = returns / gross_revenue,
+           gross_margin = (gross_revenue + total_adjustments - cogs - returns)
+           / (gross_revenue + total_adjustments - returns),
+           returns_per_unit = returns / units_shipped) %>%
+    gather(metric, value, -ship_year) %>%
+    mutate(year_colname = paste("actuals", ship_year, sep = "_")) %>%
+    ungroup() %>%
+    select(-ship_year) %>%
+    spread(year_colname, value) %>%
+    inner_join(annual_budget_2017, by = c("metric")) %>%
+    transmute(metric,
+              actuals_2016 = round(actuals_2016, 2),
+              actuals_2017 = round(actuals_2017, 2),
+              budget_2017  = round(budget_2017, 2),
+              percent_change_yoy = (actuals_2017 - actuals_2016) / actuals_2016,
+              percent_of_budget = round(actuals_2017 / budget_2017, 4))
 
 setwd("~/data")
