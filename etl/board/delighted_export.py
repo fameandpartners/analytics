05e82d48e30b3c4b965a686fdf10de82f7af1a19
Peter H <peterhughmorrow@gmail.com>
2017-09-07 13:06:56 -0700

    Use a while loop dummy

--- etl/board/delighted_export.py
+++ etl/board/delighted_export.py
@@ -2,15 +2,11 @@ import delighted
 import pandas as pd
 
 def pull(DKEY):
-    print('Querying Delighted NPS Responses')
     delighted.api_key = DKEY
     responses = []
     for page in range(1, 100):
-        print('Querying Page: ' + str(page))
         delighted_page = delighted.SurveyResponse.all(per_page=100,page=page)
-        if len(delighted_page) > 0:
+        while len(delighted_page) > 0:
             for response in delighted_page:
                 responses.append(dict(response))
-        else:
-            break
     return pd.DataFrame(responses)
