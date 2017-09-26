3fc808fd1152453e748d07712043217f1428579b
Peter H <peterhughmorrow@gmail.com>
2017-09-26 15:08:39 -0700

    Remove all the stuff from the etl folder

--- etl/board/spree_db/queries/first_order_dates.sql
+++
@@ -1,5 +0,0 @@
-SELECT email, min(completed_at::DATE) first_order_date
-FROM spree_orders
-WHERE completed_at IS NOT NULL
-    AND email IS NOT NULL
-GROUP BY email;
--- etl/board/spree_db/queries/payments.sql
+++
@@ -1,1 +0,0 @@
-SELECT order_id, amount p_amount FROM spree_payments WHERE state = 'completed' AND created_at >= '2015-12-01'
