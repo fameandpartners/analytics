e6877da8764ada315483b41df6d52f65cc2b0306
dmkrtchyan <davidm@fameandpartners.com>
2017-11-14 15:54:58 -0800

    Comment out eb.config and update scriipts

--- dw/Rscripts/fp_init.R
+++ dw/Rscripts/fp_init.R
@@ -1,7 +1,7 @@
 library(dbplyr)
 fp_con <- src_postgres(
-  host = Sys.getenv("FAME_HOSTNAME")
-  dbname = Sys.getenv("FAME_DB_NAME")
-  user = Sys.getenv("FAME_USERNAME")
+  host = Sys.getenv("FAME_HOSTNAME"),
+  dbname = Sys.getenv("FAME_DB_NAME"),
+  user = Sys.getenv("FAME_USERNAME"),
   password = Sys.getenv("FAME_PASSWORD")
 )
--- dw/hourly.sh
+++ dw/hourly.sh
@@ -1,13 +1,13 @@
 echo 'Running sales.R'
-Rscript Rscripts/sales.R
+sudo Rscript Rscripts/sales.R
 echo 'Running update.py'
 #python update.py
 python3 update.py production
 echo 'Running monthly_cohorts.R'
-Rscript Rscripts/board/monthly_cohorts.R
+sudo Rscript Rscripts/board/monthly_cohorts.R
 echo 'Running daily.R'
-Rscript Rscripts/board/daily.R
+sudo Rscript Rscripts/board/daily.R
 echo 'Running monthly_cohorts.R'
-Rscript Rscripts/board/monthly_cohorts.R
-python update_board.py
+sudo Rscript Rscripts/board/monthly_cohorts.R
+python3 update_board.py production
 # python update_board.py production
